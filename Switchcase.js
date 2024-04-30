4c7b68bbd075dbc7b0307581d5c0ec96fbc8f4bb
unknown <priyanka.1711kumari@gmail.com>
2024-04-30 13:11:24 +0530

    first commmit

---
+++ Switchcase.js
@@ -0,0 +1,17 @@
+
+        const grade='C';
+
+        switch(grade){
+            case 'A':
+                console.log("Very Good");
+                break;
+            case 'B':
+                console.log('good');
+                break;
+            case 'C' : 
+            console.log("Very bad");
+            break;
+
+            default:
+            console.log('It ok sometimes') ;    
+        }
---
+++ backtick.js
@@ -0,0 +1,9 @@
+// let variable = "value";
+// let interpolatedString = `This is a ${variable} interpolated string.`;
+// console.log( interpolatedString);
+ let abc=
+ `hi
+ my name is Priyanka
+       helloooooo
+ `;
+ console.log(abc);
